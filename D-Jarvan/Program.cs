a9f6d8574bb900eb08f25aee1b994c54904ae14b
xQx <xQxCPMxQx@gmail.com>
2014-12-15 23:27:31 +0200

    Updated for 4.21

--- D-Jarvan/Program.cs
+++ D-Jarvan/Program.cs
@@ -390,7 +390,7 @@ namespace D_Jarvan
         {
             var damage = 0d;
             if (_igniteSlot != SpellSlot.Unknown &&
-                _player.SummonerSpellbook.CanUseSpell(_igniteSlot) == SpellState.Ready)
+                _player.Spellbook.CanUseSpell(_igniteSlot) == SpellState.Ready)
                 damage += ObjectManager.Player.GetSummonerSpellDamage(enemy, Damage.SummonerSpell.Ignite);
             if (Items.HasItem(3077) && Items.CanUseItem(3077))
                 damage += _player.GetItemDamage(enemy, Damage.DamageItems.Tiamat);
@@ -417,10 +417,10 @@ namespace D_Jarvan
             var usesmite = _config.Item("smitecombo").GetValue<bool>();
             var itemscheck = SmiteBlue.Any(Items.HasItem) || SmiteRed.Any(Items.HasItem);
             if (itemscheck && usesmite &&
-                ObjectManager.Player.SummonerSpellbook.CanUseSpell(_smiteSlot) == SpellState.Ready &&
+                ObjectManager.Player.Spellbook.CanUseSpell(_smiteSlot) == SpellState.Ready &&
                 target.Distance(_player.Position) < _smite.Range)
             {
-                ObjectManager.Player.SummonerSpellbook.CastSpell(_smiteSlot, target);
+                ObjectManager.Player.Spellbook.CastSpell(_smiteSlot, target);
             }
         }
 
@@ -434,11 +434,11 @@ namespace D_Jarvan
             var t = SimpleTs.GetTarget(_e.Range, SimpleTs.DamageType.Magical);
             Smiteontarget(t);
             if (t != null && _config.Item("UseIgnite").GetValue<bool>() && _igniteSlot != SpellSlot.Unknown &&
-                _player.SummonerSpellbook.CanUseSpell(_igniteSlot) == SpellState.Ready)
+                _player.Spellbook.CanUseSpell(_igniteSlot) == SpellState.Ready)
             {
                 if (ComboDamage(t) > t.Health)
                 {
-                    _player.SummonerSpellbook.CastSpell(_igniteSlot, t);
+                    _player.Spellbook.CastSpell(_igniteSlot, t);
                 }
             }
             if (useR && _r.IsReady())
@@ -516,11 +516,11 @@ namespace D_Jarvan
 
             }
             if (t != null && _config.Item("UseIgnite").GetValue<bool>() && _igniteSlot != SpellSlot.Unknown &&
-                _player.SummonerSpellbook.CanUseSpell(_igniteSlot) == SpellState.Ready)
+                _player.Spellbook.CanUseSpell(_igniteSlot) == SpellState.Ready)
             {
                 if (ComboDamage(t) > t.Health)
                 {
-                    _player.SummonerSpellbook.CastSpell(_igniteSlot, t);
+                    _player.Spellbook.CastSpell(_igniteSlot, t);
                 }
             }
             if (_r.IsReady() && !_haveulti && t != null)
@@ -589,7 +589,7 @@ namespace D_Jarvan
                 _player.IssueOrder(GameObjectOrder.AttackUnit, t);
             }
             Smiteontarget(t);
-            if (_flashSlot != SpellSlot.Unknown && _player.SummonerSpellbook.CanUseSpell(_flashSlot) == SpellState.Ready)
+            if (_flashSlot != SpellSlot.Unknown && _player.Spellbook.CanUseSpell(_flashSlot) == SpellState.Ready)
             {
                 if (_e.IsReady() && _q.IsReady() && manacheck && t != null && _player.Distance(t) > _q.Range)
                 {
@@ -602,7 +602,7 @@ namespace D_Jarvan
 
                 if (t.IsValidTarget(flashDista) && !_q.IsReady())
                 {
-                    _player.SummonerSpellbook.CastSpell(_flashSlot, t.ServerPosition);
+                    _player.Spellbook.CastSpell(_flashSlot, t.ServerPosition);
                 }
             }
             UseItemes(t);
@@ -789,7 +789,7 @@ namespace D_Jarvan
         {
             foreach (
                 var spell in
-                    ObjectManager.Player.SummonerSpellbook.Spells.Where(
+                    ObjectManager.Player.Spellbook.Spells.Where(
                         spell => String.Equals(spell.Name, Smitetype(), StringComparison.CurrentCultureIgnoreCase)))
             {
                 _smiteSlot = spell.Slot;
@@ -815,7 +815,7 @@ namespace D_Jarvan
         private static void Smiteuse()
         {
             var jungle = _config.Item("ActiveJungle").GetValue<KeyBind>().Active;
-            if (ObjectManager.Player.SummonerSpellbook.CanUseSpell(_smiteSlot) != SpellState.Ready) return;
+            if (ObjectManager.Player.Spellbook.CanUseSpell(_smiteSlot) != SpellState.Ready) return;
             var useblue = _config.Item("Useblue").GetValue<bool>();
             var usered = _config.Item("Usered").GetValue<bool>();
             var health = (100 * (_player.Mana / _player.MaxMana)) < _config.Item("healthJ").GetValue<Slider>().Value;
@@ -844,24 +844,24 @@ namespace D_Jarvan
                         minion.Health <= smiteDmg &&
                         jungleMinions.Any(name => minion.Name.Substring(0, minion.Name.Length - 5).Equals(name)))
                     {
-                        ObjectManager.Player.SummonerSpellbook.CastSpell(_smiteSlot, minion);
+                        ObjectManager.Player.Spellbook.CastSpell(_smiteSlot, minion);
                     }
                     if (minion.Health <= smiteDmg && jungleMinions.Any(name => minion.Name.StartsWith(name)) &&
                         !jungleMinions.Any(name => minion.Name.Contains("Mini")))
                     {
-                        ObjectManager.Player.SummonerSpellbook.CastSpell(_smiteSlot, minion);
+                        ObjectManager.Player.Spellbook.CastSpell(_smiteSlot, minion);
                     }
                     else if (jungle && useblue && mana && minion.Health >= smiteDmg &&
                              jungleMinions.Any(name => minion.Name.StartsWith("SRU_Blue")) &&
                              !jungleMinions.Any(name => minion.Name.Contains("Mini")))
                     {
-                        ObjectManager.Player.SummonerSpellbook.CastSpell(_smiteSlot, minion);
+                        ObjectManager.Player.Spellbook.CastSpell(_smiteSlot, minion);
                     }
                     else if (jungle && usered && health && minion.Health >= smiteDmg &&
                              jungleMinions.Any(name => minion.Name.StartsWith("SRU_Red")) &&
                              !jungleMinions.Any(name => minion.Name.Contains("Mini")))
                     {
-                        ObjectManager.Player.SummonerSpellbook.CastSpell(_smiteSlot, minion);
+                        ObjectManager.Player.Spellbook.CastSpell(_smiteSlot, minion);
                     }
                 }
             }
@@ -930,11 +930,11 @@ namespace D_Jarvan
             var target = SimpleTs.GetTarget(_q.Range, SimpleTs.DamageType.Magical);
             var igniteDmg = _player.GetSummonerSpellDamage(target, Damage.SummonerSpell.Ignite);
             if (target != null && _config.Item("UseIgnitekill").GetValue<bool>() && _igniteSlot != SpellSlot.Unknown &&
-                _player.SummonerSpellbook.CanUseSpell(_igniteSlot) == SpellState.Ready)
+                _player.Spellbook.CanUseSpell(_igniteSlot) == SpellState.Ready)
             {
                 if (igniteDmg > target.Health)
                 {
-                    _player.SummonerSpellbook.CastSpell(_igniteSlot, target);
+                    _player.Spellbook.CastSpell(_igniteSlot, target);
                 }
             }
             if (_q.IsReady() && _config.Item("UseQM").GetValue<bool>())
