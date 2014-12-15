3c4ab42753b390156058d81415b61ce245956441
xQx <xQxCPMxQx@gmail.com>
2014-12-15 23:29:59 +0200

    Updated for 4.21

--- D-Udyr/Program.cs
+++ D-Udyr/Program.cs
@@ -484,10 +484,10 @@ namespace D_Udyr
             var usesmite = _config.Item("smitecombo").GetValue<bool>();
             var itemscheck = SmiteBlue.Any(Items.HasItem) || SmiteRed.Any(Items.HasItem);
             if (itemscheck && usesmite &&
-                ObjectManager.Player.SummonerSpellbook.CanUseSpell(_smiteSlot) == SpellState.Ready &&
+                ObjectManager.Player.Spellbook.CanUseSpell(_smiteSlot) == SpellState.Ready &&
                 target.Distance(_player.Position) < _smite.Range)
             {
-                ObjectManager.Player.SummonerSpellbook.CastSpell(_smiteSlot, target);
+                ObjectManager.Player.Spellbook.CastSpell(_smiteSlot, target);
             }
         }
         private static void Combo()
@@ -612,7 +612,7 @@ namespace D_Udyr
         {
             foreach (
                 var spell in
-                    ObjectManager.Player.SummonerSpellbook.Spells.Where(
+                    ObjectManager.Player.Spellbook.Spells.Where(
                         spell => String.Equals(spell.Name, Smitetype(), StringComparison.CurrentCultureIgnoreCase)))
             {
                 _smiteSlot = spell.Slot;
@@ -632,7 +632,7 @@ namespace D_Udyr
         private static void Smiteuse()
         {
             var jungle = _config.Item("ActiveJungle").GetValue<KeyBind>().Active;
-            if (ObjectManager.Player.SummonerSpellbook.CanUseSpell(_smiteSlot) != SpellState.Ready) return;
+            if (ObjectManager.Player.Spellbook.CanUseSpell(_smiteSlot) != SpellState.Ready) return;
             var useblue = _config.Item("Useblue").GetValue<bool>();
             var usered = _config.Item("Usered").GetValue<bool>();
             var health = (100 * (_player.Mana / _player.MaxMana)) < _config.Item("healthJ").GetValue<Slider>().Value;
@@ -661,24 +661,24 @@ namespace D_Udyr
                         minion.Health <= smiteDmg &&
                         jungleMinions.Any(name => minion.Name.Substring(0, minion.Name.Length - 5).Equals(name)))
                     {
-                        ObjectManager.Player.SummonerSpellbook.CastSpell(_smiteSlot, minion);
+                        ObjectManager.Player.Spellbook.CastSpell(_smiteSlot, minion);
                     }
                     if (minion.Health <= smiteDmg && jungleMinions.Any(name => minion.Name.StartsWith(name)) &&
                         !jungleMinions.Any(name => minion.Name.Contains("Mini")))
                     {
-                        ObjectManager.Player.SummonerSpellbook.CastSpell(_smiteSlot, minion);
+                        ObjectManager.Player.Spellbook.CastSpell(_smiteSlot, minion);
                     }
                     else if (jungle && useblue && mana && minion.Health >= smiteDmg &&
                              jungleMinions.Any(name => minion.Name.StartsWith("SRU_Blue")) &&
                              !jungleMinions.Any(name => minion.Name.Contains("Mini")))
                     {
-                        ObjectManager.Player.SummonerSpellbook.CastSpell(_smiteSlot, minion);
+                        ObjectManager.Player.Spellbook.CastSpell(_smiteSlot, minion);
                     }
                     else if (jungle && usered && health && minion.Health >= smiteDmg &&
                              jungleMinions.Any(name => minion.Name.StartsWith("SRU_Red")) &&
                              !jungleMinions.Any(name => minion.Name.Contains("Mini")))
                     {
-                        ObjectManager.Player.SummonerSpellbook.CastSpell(_smiteSlot, minion);
+                        ObjectManager.Player.Spellbook.CastSpell(_smiteSlot, minion);
                     }
                 }
             }
